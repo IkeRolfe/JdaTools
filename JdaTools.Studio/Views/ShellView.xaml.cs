1299f9c97c605cbddbb38517284b9dbf57175d3a
Isaac Rolfe <Isaac.Rolfe@ecentria.com>
2021-07-29 16:20:00 -0600

    intital mah implementation

--- JdaTools.Studio/Views/ShellView.xaml.cs
+++ JdaTools.Studio/Views/ShellView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MahApps.Metro.Controls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,7 +18,7 @@ namespace JdaTools.Studio.Views
     /// <summary>
     /// Interaction logic for ShellView.xaml
     /// </summary>
-    public partial class ShellView : Window
+    public partial class ShellView : MetroWindow
     {
         public ShellView()
         {
